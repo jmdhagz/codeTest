1ac12630d240d069f399761db606f47a89333f93
Journey Dagoc <jmdhagz@gmail.com>
2022-01-28 11:51:03 +0800

    landing page changes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,23 +6,29 @@
         <div class="col-md-12">
             <h1>Tweets</h1>
             <ul class="list-group" style="margin-top: 10px;">
-                @foreach($post_list as $post)
-                    <li class="list-group-item">
-                        <div class="media">
-                            <img src="{{ asset('images/avatar.png') }}" class="mr-3" alt="..." style="width: 50px;">
-                            <div class="media-body">
-                                @if($post -> users_id == Auth::user()->id)
-                                    <a href="{{ url('posts') }}" style="text-decoration: none; color: #263238;"><h5 class="mt-0">{{ $post -> name }} (You)</h5></a>
-                                @else
-                                    <h5 class="mt-0">{{ $post -> name }}</h5>
-                                @endif
-                                <p style="margin-bottom: 5px;">{{ $post -> description }}</p>
-                                <p style="color: #6C757D; font-size: 12px; margin-bottom: 0px;">{{ date('h:i', strtotime($post -> created_at)) }} • {{ date('m/d/y', strtotime($post -> created_at)) }}</p>
+                @if(count($post_list) > 0)
+                    @foreach($post_list as $post)
+                        <li class="list-group-item">
+                            <div class="media">
+                                <img src="{{ asset('images/avatar.png') }}" class="mr-3" alt="..." style="width: 50px;">
+                                <div class="media-body">
+                                    @if($post -> users_id == Auth::user()->id)
+                                        <a href="{{ url('posts') }}" style="text-decoration: none; color: #263238;"><h5 class="mt-0">{{ $post -> name }} (You)</h5></a>
+                                    @else
+                                        <h5 class="mt-0">{{ $post -> name }}</h5>
+                                    @endif
+                                    <p style="margin-bottom: 5px;">{{ $post -> description }}</p>
+                                    <p style="color: #6C757D; font-size: 12px; margin-bottom: 0px;">{{ date('h:i', strtotime($post -> created_at)) }} • {{ date('m/d/y', strtotime($post -> created_at)) }}</p>
+                                </div>
+                                <p style="color: #6C757D;">{{ ($post -> remark == 1) ? 'Private' : 'Public' }}</p>
                             </div>
-                            <p style="color: #6C757D;">{{ ($post -> remark == 1) ? 'Private' : 'Public' }}</p>
-                        </div>
-                    </li>
-                @endforeach
+                        </li>
+                    @endforeach
+                @else
+                    <div class="alert alert-secondary" role="alert">
+                        <p class="lead" style="text-align: center; margin-bottom: 0px;">Empty</p>
+                    </div>
+                @endif
             </ul>
         </div>
     </div>
